cc73b21db00ffe459569a19affc9d055d1194353
Shahnewaz Ayon <ayonmahmud53@gmail.com>
2022-01-10 21:53:58 +0600

    till 4. Rendering Routes video

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
-
 import MainComponent from './Components/MainComponent';
-
-
-// function App() {
-//   return (
-//     <div className="App">
-
-//       <h1> Hello World </h1>
-//       <Person />
-
-//     </div>
-//   );
-// }
+import { BrowserRouter } from 'react-router-dom';
 
 const App = () => {
-  return <MainComponent />
+  return (
+    <BrowserRouter>
+      <MainComponent />
+    </BrowserRouter>
+  );
 }
 
 export default App;
